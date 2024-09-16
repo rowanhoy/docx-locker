3f1b8f06dfce7d51b566060745123596f9de001e
RH1 <rowan.hoy@outlook.com>
2024-09-16 21:27:43 +1000

    imrpove outputs, docs

--- docx_locker/__init__.py
+++ docx_locker/__init__.py
@@ -1,5 +1,5 @@
-from .docx_locker import apply_docx_protection, get_docx_protection
+from .docx_locker import apply_docx_protection, get_docx_protection, DocxProtectionParams
 
-__all__ = ["apply_docx_protection", "get_docx_protection"]
+__all__ = ["apply_docx_protection", "get_docx_protection", "DocxProtectionParams"]
 
 __version__ = "0.6.0"
