93c2c5aa220bd01d12be2c66398b38e0b501bbcb
RH1 <90246448+rowanhoy@users.noreply.github.com>
2025-10-10 06:31:13 +0000

    Bump version: 0.7.1 → 0.7.2

--- docx_locker/__init__.py
+++ docx_locker/__init__.py
@@ -2,4 +2,4 @@ from .docx_locker import apply_docx_protection, apply_docx_protection_buffer, ge
 
 __all__ = ["apply_docx_protection", "apply_docx_protection_buffer", "get_docx_protection", "DocxProtectionParams"]
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
